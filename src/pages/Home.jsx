e323d78af61a8cd97a7455f2f63571281f1f732e
NeoXsa <neoxsa8@gmail.com>
2026-01-09 23:09:04 +0530

    Add HeroSection component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import HeroSection from '#components/HeroSection'
 import React from 'react'
 
 export default function Home() {
     return (
         <div className='flex flex-col items-center justify-center h-screen'>
             <h1 className='text-4xl text-white font-bold'>Welcome to the Home Page</h1>
+            <HeroSection />
         </div>
     )
 }
