d5b89553ee08b48eab79539198223e60db4604cf
NeoXsa <neoxsa8@gmail.com>
2026-01-13 23:54:48 +0530

    Add Skeleton component and error handling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroSkeleton from './Skeleton/HeroSkeleton';
 import { useGetAllTrendingQuery } from '#api/tmdbApi.js'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
@@ -10,73 +11,76 @@ import 'swiper/css/effect-fade';
 
 function HeroSection() {
 
-    const { data } = useGetAllTrendingQuery();
-
-    const trendingMovies = data?.results|| [];
+    const { data, isLoading, isError, error } = useGetAllTrendingQuery();
+    const trendingMovies = data?.results || [];
     console.log(trendingMovies);
 
     return (
         <section className="relative min-h-[70vh] w-full overflow-hidden">
             <div className="w-full h-full">
-                {!trendingMovies.length ? null : (
-                    <Swiper
-                        modules={[Pagination, Autoplay, EffectFade, Navigation]}
-                        effect="fade"
-                        fadeEffect={{ crossFade: true }}
-                        autoplay={{
-                            delay: 5000,
-                            disableOnInteraction: false,
-                        }}
-                        pagination={{
-                            clickable: true,
-                            dynamicBullets: true,
-                        }}
-                        spaceBetween={20}
-                        slidesPerView={1}
-                        loop={true}
-                    >
-                        {trendingMovies.map((movie) => (
-                            <SwiperSlide key={movie.id} className="relative">
-                                {/* Background image */}
-                                <img
-                                    src={`https://image.tmdb.org/t/p/w1280/${movie?.backdrop_path}`}
-                                    alt={movie?.name || movie?.title}
-                                    className="w-full h-[70vh] xl:h-[90vh] object-cover"
-                                    loading="lazy"
-                                    decoding="async"
-                                />
+                {
+                    isLoading ? (
+                        <HeroSkeleton />
+                    ) :
+                        isError ? <div className='text-red-500'>Error: {error.message}</div> :
+                            (<Swiper
+                                modules={[Pagination, Autoplay, EffectFade, Navigation]}
+                                effect="fade"
+                                fadeEffect={{ crossFade: true }}
+                                autoplay={{
+                                    delay: 5000,
+                                    disableOnInteraction: false,
+                                }}
+                                pagination={{
+                                    clickable: true,
+                                    dynamicBullets: true,
+                                }}
+                                spaceBetween={20}
+                                slidesPerView={1}
+                                loop={true}
+                            >
+                                {trendingMovies.map((movie) => (
+                                    <SwiperSlide key={movie.id} className="relative">
+                                        {/* Background image */}
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w1280/${movie?.backdrop_path}`}
+                                            alt={movie?.name || movie?.title}
+                                            className="w-full h-[70vh] xl:h-[90vh] object-cover"
+                                            loading="lazy"
+                                            decoding="async"
+                                        />
 
-                                <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-transparent" />
+                                        <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/40 to-transparent" />
 
-                                {/* Content */}
-                                <div className="absolute inset-0 flex items-center">
-                                    <div className="px-6 md:px-16 max-w-2xl text-white space-y-4 ">
-                                        <h1 className="text-3xl md:text-5xl font-extrabold text-red-400 leading-tight">
-                                            {movie?.name || movie?.original_name || movie?.title}
-                                        </h1>
+                                        {/* Content */}
+                                        <div className="absolute inset-0 flex items-center">
+                                            <div className="px-6 md:px-16 max-w-2xl text-white space-y-4 ">
+                                                <h1 className="text-3xl md:text-5xl font-extrabold text-red-400 leading-tight">
+                                                    {movie?.name || movie?.original_name || movie?.title}
+                                                </h1>
 
-                                        <div className="flex items-center gap-4 text-sm md:text-base text-gray-300">
-                                            <span className="font-semibold">
-                                                ⭐ {movie?.vote_average?.toFixed(1)}
-                                            </span>
-                                            <span className="uppercase tracking-wide">
-                                                {movie?.media_type}
-                                            </span>
+                                                <div className="flex items-center gap-4 text-sm md:text-base text-gray-300">
+                                                    <span className="font-semibold">
+                                                        ⭐ {movie?.vote_average?.toFixed(1)}
+                                                    </span>
+                                                    <span className="uppercase tracking-wide">
+                                                        {movie?.media_type}
+                                                    </span>
+                                                </div>
+
+                                                <p className="text-sm md:text-lg text-gray-200 line-clamp-4">
+                                                    {movie?.overview}
+                                                </p>
+
+                                                <button className="mt-4 inline-flex items-center gap-2 bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-lg text-black font-bold cursor-pointer">
+                                                    Watch Now
+                                                </button>
+                                            </div>
                                         </div>
-
-                                        <p className="text-sm md:text-lg text-gray-200 line-clamp-4">
-                                            {movie?.overview}
-                                        </p>
-
-                                        <button className="mt-4 inline-flex items-center gap-2 bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-lg text-black font-bold cursor-pointer">
-                                            Watch Now
-                                        </button>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                )}
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                            )}
             </div>
         </section>
 
