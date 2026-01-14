8957e68ac377626cb1db4a0a0f4b292116256457
NeoXsa <neoxsa8@gmail.com>
2026-01-14 23:06:07 +0530

    Implement data fetching and Movies Slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,30 @@
 import HeroSection from '#components/HeroSection'
 import MoviesSlider from '#components/MoviesSlider'
+import { useGetAllTrendingQuery, useGetDiscoverMoviesQuery } from "#api/tmdbApi"
 import React from 'react'
 
 export default function Home() {
+    const { data: discoverMoviesData, isLoading, isError, error } = useGetDiscoverMoviesQuery();
+    const { data: trendingMoviesData,  } = useGetAllTrendingQuery();
+    const discoverMovies = discoverMoviesData?.results ?? []
+    const trendingMovies = trendingMoviesData?.results ?? []
+
     return (
         <div className='flex flex-col'>
             <HeroSection />
 
-            <MoviesSlider />
+            {/* Trending Movies */}
+            <MoviesSlider
+                title={"Trending Movies"}
+                movies={trendingMovies}
+            />
+
+            {/* Discover Movies */}
+            <MoviesSlider
+                title={"Discover Movies"}
+                movies={discoverMovies}
+            />
+
         </div>
     )
 }
