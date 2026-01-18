347f8a5f394b2689fa8d0024722409596b23c180
NeoXsa <neoxsa8@gmail.com>
2026-01-18 22:55:30 +0530

    add DetailView component

---
+++ src/pages/DetailView.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSectionViewPage from '#components/HeroSectionViewPage'
+
+function DetailView() {
+  return (
+    <>
+    <HeroSectionViewPage /> 
+    </>
+  )
+}
+
+export default DetailView
