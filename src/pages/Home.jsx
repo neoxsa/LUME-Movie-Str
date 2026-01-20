d52ae75c1bed0b19343cf44a90a8667b29fa7e02
NeoXsa <neoxsa8@gmail.com>
2026-01-20 22:43:51 +0530

    feat: add MovieSlider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,38 @@
 import HeroSection from '#components/HeroSection'
 import MoviesSlider from '#components/MoviesSlider'
-import { useGetAllTrendingQuery, useGetDiscoverMoviesQuery } from "#api/tmdbApi"
+import { useGetAllTrendingQuery, useGetDiscoverMoviesQuery, useGetDiscoverTVShowsQuery } from "#api/tmdbApi"
 import React from 'react'
 
 export default function Home() {
-    const { data: discoverMoviesData, isLoading, isError, error } = useGetDiscoverMoviesQuery();
-    const { data: trendingMoviesData,  } = useGetAllTrendingQuery();
+    const { data: discoverMoviesData } = useGetDiscoverMoviesQuery(1);
+    const { data: discoverTVShowsData } = useGetDiscoverTVShowsQuery(1);
+    const { data: trendingData, } = useGetAllTrendingQuery();
     const discoverMovies = discoverMoviesData?.results ?? []
-    const trendingMovies = trendingMoviesData?.results ?? []
+    const discoverTVShows = discoverTVShowsData?.results ?? []
+    const trending = trendingData?.results ?? []
 
     return (
         <div className='flex flex-col'>
             <HeroSection />
 
-            {/* Trending Movies */}
+            {/* Trending Movies & TV Shows */}
             <MoviesSlider
-                title={"Trending Movies"}
-                movies={trendingMovies}
+                title={"Trending Movies & TV Shows"}
+                movies={trending}
+                btnClickAction={"/"}
             />
 
             {/* Discover Movies */}
             <MoviesSlider
                 title={"Discover Movies"}
                 movies={discoverMovies}
+                btnClickAction={'/movies'}
+            />
+            {/* Discover TV Shows */}
+            <MoviesSlider
+                title={"Discover TV Shows"}
+                movies={discoverTVShows}
+                btnClickAction={'/tv-shows'}
             />
 
         </div>
