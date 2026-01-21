6e38b9f12e43c1e8c912bc9b974fadf72abc1f8c
NeoXsa <neoxsa8@gmail.com>
2026-01-21 23:49:19 +0530

    Add detail view component

--- src/pages/DetailView.jsx
+++ src/pages/DetailView.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import HeroSectionViewPage from '#components/HeroSectionViewPage'
+import { useParams } from 'react-router-dom'
+import { useGetMovieTrailerQuery, useGetMoviesByIDQuery, useGetTVShowTrailerQuery, useGetTVShowsByIDQuery , useGetMovieCreditsQuery, useGetTVCreditsQuery} from '#api/tmdbApi'
+import Casts from '#components/Casts';
 
 function DetailView() {
+  const { id } = useParams();
+
+  const selectedId = Number(id);
+  console.log("Selected ID:", selectedId);
+
+  // Movies Id
+  const { data: selectedData } = useGetMoviesByIDQuery(selectedId)
+  console.log("Selected Data:", selectedData);
+  // TV Shows Id
+  // const { data: selectedTVData, isLoading: isTVLoading, isError: isTVError } = useGetTVShowsByIDQuery(selectedId)
+  // // Movie Trailer
+  // const { data: trailerData, isLoading: isTrailerLoading, isError: isTrailerError } = useGetMovieTrailerQuery(selectedId)
+  // // TV Show Trailer
+  // const { data: tvTrailerData, isLoading: isTVTrailerLoading, isError: isTVTrailerError } = useGetTVShowTrailerQuery({ series_id: selectedId, season_number: 1 })
+  const { data: movieCredits } = useGetMovieCreditsQuery(selectedId);
+
+  console.log("Movie Credits:", movieCredits?.cast);
+
+  // const { data: tvCredits } = useGetTVCreditsQuery(selectedId);
+
+  // console.log("TV Credits:", tvCredits);
+
   return (
     <>
-    <HeroSectionViewPage /> 
+      <HeroSectionViewPage
+        selectedData={selectedData}
+      />
+      <Casts
+      cast={movieCredits?.cast} 
+      />
     </>
   )
 }
