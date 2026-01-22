e9f35fd750ae14def538c02e460d135af6cdfd1e
NeoXsa <neoxsa8@gmail.com>
2026-01-22 23:03:48 +0530

    feat: intigrate trailer component

--- src/components/HeroSectionViewPage.jsx
+++ src/components/HeroSectionViewPage.jsx
@@ -1,21 +1,37 @@
+import { Play } from 'lucide-react';
 import React from 'react'
+import TrailerOnYT from './TrailerOnYT';
 
 function HeroSectionViewPage({
     selectedData,
+    trailerKey
 }) {
 
+    const [isTrailerOpen, setIsTrailerOpen] = React.useState(false);
+
+    console.log(isTrailerOpen);
 
     return (
         <section className='relative -top-17 w-full h-[90vh] overflow-hidden'>
 
-            <div className='relative w-full h-full mt-4' key={selectedData?.id}>
+            {
+                isTrailerOpen && (
+                    <TrailerOnYT 
+                    trailerKey={trailerKey} 
+                    onClose={() => setIsTrailerOpen(false)} />
+                )
+            }
+
+            <div 
+            className='relative w-full h-full mt-4' 
+            key={selectedData?.id}>
                 <img
                     className='absolute inset-0 w-full h-full object-cover'
                     src={`https://image.tmdb.org/t/p/w1280/${selectedData?.backdrop_path}`}
                     alt={selectedData?.name || selectedData?.title}
                 />
 
-                <div className='absolute inset-0 bg-linear-to-r from-black/80 via-black/50 to-transparent' />
+                < div className='absolute inset-0 bg-linear-to-r from-black/80 via-black/50 to-transparent' />
 
                 <div className='relative z-10 flex items-center h-full px-6 md:px-16 lg:px-24'>
                     <div className='flex flex-col lg:flex-row items-center lg:items-start gap-8 lg:gap-12 max-w-7xl'>
@@ -25,12 +41,11 @@ function HeroSectionViewPage({
                                 src={`https://image.tmdb.org/t/p/w400/${selectedData?.poster_path}`}
                                 alt={selectedData?.name || selectedData?.title}
                             />
-                            <button className='absolute inset-0 flex items-center justify-center bg-black/40 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity'>
+                            <button
+                                onClick={() => setIsTrailerOpen(!isTrailerOpen)}
+                                className='absolute inset-0 flex items-center justify-center bg-black/40 rounded-2xl opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer'>
                                 <div className='w-16 h-16 bg-red-600 rounded-full flex items-center justify-center hover:bg-red-700 transition-colors shadow-lg'>
-                                    <svg
-                                        className='w-6 h-6 text-white ml-1' fill='currentColor' viewBox='0 0 24 24'>
-                                        <path d='M8 5v14l11-7z' />
-                                    </svg>
+                                    <Play className='w-6 h-6 text-white ml-1' />
                                 </div>
                             </button>
                         </div>
@@ -43,7 +58,14 @@ function HeroSectionViewPage({
                             <div className='flex items-center gap-4 text-sm md:text-base text-gray-300'>
                                 <span>{selectedData?.release_date || selectedData?.first_air_date}</span>
                                 <span>•</span>
+                                {
+                                    !selectedData || !selectedData.genres ? null :
+                                        <span className='font-medium'>
+                                            {selectedData.genres.map(g => g.name).join(", ")}
+                                        </span>
+                                }
                                 <div className='flex items-center gap-2'>
+                                    <span>•</span>
                                     <span>⭐</span>
                                     <span className='font-semibold text-yellow-400'>{selectedData?.vote_average?.toFixed(1)}</span>
                                     <span className='text-gray-400'>/10</span>
@@ -63,8 +85,8 @@ function HeroSectionViewPage({
 
                     </div>
                 </div>
-            </div>
-        </section>
+            </div >
+        </section >
     )
 }
 
