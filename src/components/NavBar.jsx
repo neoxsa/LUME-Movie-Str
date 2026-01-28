c370f58827d65e84e319fb36df727336798d5c72
NeoXsa <neoxsa8@gmail.com>
2026-01-28 22:56:57 +0530

    add SideNavBar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import logo from '#assets/logo.webp'
+import { Menu, X } from 'lucide-react'
+import SideNavBar from './SideNavBar'
 
 function NavBar() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    console.log(isMenuOpen)
+
     const navItems = [
         {
             id: 1,
@@ -36,37 +47,64 @@ function NavBar() {
                 </Link>
 
                 {/* Nav links */}
-                <ul className="hidden md:flex items-center gap-8">
-                    {navItems.map((item) => (
-                        <li key={item.id} className="relative group">
-                            <NavLink
-                                to={item.to}
-                                className={({ isActive }) => `text-base font-medium transition-colors duration-200 group-hover:text-white ${isActive ? 'font-bold text-red-400/90' : "text-white/90"}`}
-                            >
-                                {item.label}
-                            </NavLink>
 
-                            <span
-                                className="absolute -bottom-1 left-0 h-0.5 w-0 bg-[#df2144] transition-all duration-300 group-hover:w-full"
+                <div className='flex justify-center items-center gap-0 sm:gap-5'>
+                    <div className='md:hidden'>
+                        {
+                            isMenuOpen ? (
+                                <X
+                                    onClick={toggleMenu}
+                                />
+                            ) : (
+                                <Menu
+                                    onClick={toggleMenu}
+                                />
+                            )
+                        }
+                    </div>
+
+                    {
+                        isMenuOpen && (
+                            <SideNavBar
+                            navItems={navItems}
                             />
-                        </li>
-                    ))}
-                </ul>
+                        )
+                    }
 
-                {/* Buttons */}
-                <div className="flex items-center gap-3">
-                    <button
-                        className=" hidden sm:inline-flex px-4 py-2  text-white/90 border border-white/20  bg-white/5 rounded-lg  hover:bg-white/10 transition text-sm font-medium"
-                    >
-                        Log In
-                    </button>
+                    <ul className="hidden md:flex items-center gap-8">
+                        {navItems.map((item) => (
+                            <li key={item.id} className="relative group">
+                                <NavLink
+                                    to={item.to}
+                                    className={({ isActive }) => `text-base font-medium transition-colors duration-200 group-hover:text-white ${isActive ? 'font-bold text-red-400/90' : "text-white/90"}`}
+                                >
+                                    {item.label}
+                                </NavLink>
 
-                    <button
-                        className=" px-4 py-2 bg-[#df2144]  text-black rounded-lg font-semibold text-sm  hover:bg-[#c81c3b] transition"
-                    >
-                        Sign Up
-                    </button>
+                                <span
+                                    className="absolute -bottom-1 left-0 h-0.5 w-0 bg-[#df2144] transition-all duration-300 group-hover:w-full"
+                                />
+                            </li>
+                        ))}
+                    </ul>
+
+                    {/* Buttons */}
+                    <div className="flex items-center gap-3">
+                        <button
+                            className=" hidden md:inline-flex px-4 py-2  text-white/90 border border-white/20  bg-white/5 rounded-lg  hover:bg-white/10 transition text-sm font-medium"
+                        >
+                            Log In
+                        </button>
+
+                        <button
+                            className="hidden md:inline-flex px-4 py-2 bg-[#df2144]  text-black rounded-lg font-semibold text-sm  hover:bg-[#c81c3b] transition"
+                        >
+                            Sign Up
+                        </button>
+                    </div>
                 </div>
+
+
             </div>
         </nav>
 
