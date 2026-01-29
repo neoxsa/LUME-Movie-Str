6b67c32e79901860c2f34592901a8c4c17d92c62
NeoXsa <neoxsa8@gmail.com>
2026-01-29 20:18:47 +0530

    refactor: update swiper code and add keyboard & mousewheel

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { useRef, useEffect } from 'react'
 import HeroSkeleton from './Skeleton/HeroSkeleton';
 import { useNavigate } from 'react-router-dom';
 import { useGetAllTrendingQuery } from '#api/tmdbApi.js'
-import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade, Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { ArrowBigLeftDash, ArrowBigRightDash } from "lucide-react"
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 
@@ -14,6 +14,12 @@ function HeroSection() {
     const { data, isLoading, isError, error } = useGetAllTrendingQuery();
     const navigate = useNavigate();
     const trendingMoviesOrShows = data?.results || [];
+
+    // Swiper 
+    const swiperRef = useRef(null);
+    const prevButtonRef = useRef(null);
+    const nextButtonRef = useRef(null);
+
 
 
     return (
@@ -24,71 +30,87 @@ function HeroSection() {
                         <HeroSkeleton />
                     ) :
                         isError ? <div className='text-red-500'>Error: {error.message}</div> :
-                            (<Swiper
-                                modules={[Pagination, Autoplay, EffectFade, Navigation]}
-                                effect="fade"
-                                fadeEffect={{ crossFade: true }}
-                                autoplay={{
-                                    delay: 5000,
-                                    disableOnInteraction: false,
-                                }}
-                                pagination={{
-                                    clickable: true,
-                                    dynamicBullets: true,
-                                }}
-                                navigation={{
-                                    clickable: true,
+                            (
+                                <>
+                                    <Swiper
+                                        ref={swiperRef}
+                                        modules={[Pagination, Autoplay, EffectFade, Navigation, Mousewheel, Keyboard]}
+                                        effect="fade"
+                                        fadeEffect={{ crossFade: true }}
+                                        autoplay={{
+                                            delay: 5000,
+                                            disableOnInteraction: false,
+                                        }}
+                                        pagination={{
+                                            clickable: true,
+                                            dynamicBullets: true,
+                                        }}
+                                        onBeforeInit={(swiper) => {
+                                            swiper.params.navigation.prevEl = prevButtonRef.current;
+                                            swiper.params.navigation.nextEl = nextButtonRef.current;
+                                        }}
+                                        keyboard={{
+                                            enabled: true,
+                                            onlyInViewport: true, 
+                                        }}
+                                        mousewheel={{
+                                            forceToAxis: true,    
+                                            sensitivity: 1,
+                                            releaseOnEdges: true,
+                                        }}
+                                        spaceBetween={20}
+                                        slidesPerView={1}
+                                        loop={true}
+                                    >
+                                        {trendingMoviesOrShows.map((media) => (
+                                            <SwiperSlide key={media.id} className="relative">
 
-                                }}
-                                spaceBetween={20}
-                                slidesPerView={1}
-                                loop={true}
-                            >
-                                {trendingMoviesOrShows.map((media) => (
-                                    <SwiperSlide key={media.id} className="relative">
+                                                {/* Background image */}
+                                                <img
+                                                    src={`https://image.tmdb.org/t/p/w1280/${media?.backdrop_path}`}
+                                                    alt={media?.name || media?.title}
+                                                    className="w-full h-[70vh] xl:h-[90vh] object-cover"
+                                                    loading="lazy"
+                                                    decoding="async"
+                                                />
 
-                                        {/* Background image */}
-                                        <img
-                                            src={`https://image.tmdb.org/t/p/w1280/${media?.backdrop_path}`}
-                                            alt={media?.name || media?.title}
-                                            className="w-full h-[70vh] xl:h-[90vh] object-cover"
-                                            loading="lazy"
-                                            decoding="async"
-                                        />
+                                                <div className="absolute inset-0 bg-linear-to-r from-black/80 via-black/40 to-transparent" />
 
-                                        <div className="absolute inset-0 bg-linear-to-r from-black/80 via-black/40 to-transparent" />
+                                                {/* Content */}
+                                                <div className="absolute inset-0 flex items-center md:mx-10 mt-20 md:mt-0">
+                                                    <div className="px-6 md:px-16 max-w-2xl text-white space-y-4 ">
+                                                        <h1 className="text-3xl md:text-5xl font-extrabold text-red-400 leading-tight">
+                                                            {media?.name || media?.original_name || media?.title}
+                                                        </h1>
 
-                                        {/* Content */}
-                                        <div className="absolute inset-0 flex items-center mt-20 md:mt-0">
-                                            <div className="px-6 md:px-16 max-w-2xl text-white space-y-4 ">
-                                                <h1 className="text-3xl md:text-5xl font-extrabold text-red-400 leading-tight">
-                                                    {media?.name || media?.original_name || media?.title}
-                                                </h1>
+                                                        <div className="flex items-center gap-4 text-sm md:text-base text-gray-300">
+                                                            <span className="font-semibold">
+                                                                ⭐ {media?.vote_average?.toFixed(1)}
+                                                            </span>
+                                                            <span className="uppercase tracking-wide">
+                                                                {media?.media_type}
+                                                            </span>
+                                                        </div>
 
-                                                <div className="flex items-center gap-4 text-sm md:text-base text-gray-300">
-                                                    <span className="font-semibold">
-                                                        ⭐ {media?.vote_average?.toFixed(1)}
-                                                    </span>
-                                                    <span className="uppercase tracking-wide">
-                                                        {media?.media_type}
-                                                    </span>
+                                                        <p className="text-sm md:text-lg text-gray-200 line-clamp-4">
+                                                            {media?.overview}
+                                                        </p>
+
+                                                        <button
+                                                            onClick={() => navigate(`/category/${media?.media_type}/${media?.id}`)}
+                                                            className="mt-4 inline-flex items-center gap-2 bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-lg text-black font-bold cursor-pointer">
+                                                            Watch Now
+                                                        </button>
+                                                    </div>
                                                 </div>
-
-                                                <p className="text-sm md:text-lg text-gray-200 line-clamp-4">
-                                                    {media?.overview}
-                                                </p>
-
-                                                <button
-                                                    onClick={() => navigate(`/category/${media?.media_type}/${media?.id}`)}
-                                                    className="mt-4 inline-flex items-center gap-2 bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-lg text-black font-bold cursor-pointer">
-                                                    Watch Now
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
+                                            </SwiperSlide>
+                                        ))}
+                                    </Swiper>
+                                    <button ref={prevButtonRef} className="custom-prev"><ArrowBigLeftDash className="text-red-400" /></button>
+                                    <button ref={nextButtonRef} className="custom-next"><ArrowBigRightDash className="text-red-400" /></button>
+                                </>
                             )}
+
             </div>
         </section>
 
