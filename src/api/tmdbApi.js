21335d5fa22b061e19d5754fed9484bf4d9a5d5d
NeoXsa <neoxsa8@gmail.com>
2026-01-30 23:41:41 +0530

    feat: add search query

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -20,12 +20,17 @@ export const apiSlice = createApi({
             query: () => `trending/all/week`
         }),
 
-        // Discover Movies
+        // Search Movies/TV Shows
+        getSearchResults: builder.query({
+            query: ({ search_type, search_key }) => `/search/${search_type}?query=${search_key}&include_adult=false`
+        }),
+
+        // Discover Movies Pagination
         getDiscoverMovies: builder.query({
             query: (page_no) => `discover/movie?include_adult=false&include_video=false&language=en-US&page=${page_no}&sort_by=popularity.desc`
         }),
 
-        // Discover TV Shows
+        // Discover TV Shows Pagination
         getDiscoverTVShows: builder.query({
             query: (page_no) => `discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page_no}&sort_by=popularity.desc`
         }),
@@ -67,5 +72,5 @@ export const apiSlice = createApi({
     })
 })
 
-export const { useGetAllTrendingQuery, useGetDiscoverMoviesQuery, useGetMoviesByIDQuery, useGetTVShowsByIDQuery, useGetMovieTrailerQuery, useGetDiscoverTVShowsQuery, useGetTVShowTrailerQuery, useGetMovieCreditsQuery, useGetTVCreditsQuery, useGetTVEpGroupQuery } = apiSlice
+export const { useGetAllTrendingQuery, useGetDiscoverMoviesQuery, useGetMoviesByIDQuery, useGetTVShowsByIDQuery, useGetMovieTrailerQuery, useGetDiscoverTVShowsQuery, useGetTVShowTrailerQuery, useGetMovieCreditsQuery, useGetTVCreditsQuery, useGetTVEpGroupQuery, useGetSearchResultsQuery } = apiSlice
 
