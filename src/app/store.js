332551a561130e705a95ef2ea7781ca5e3bd32cd
NeoXsa <neoxsa8@gmail.com>
2026-02-04 23:19:26 +0530

    refactor: implement searchResuktReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "#api/tmdbApi";
+import { searchResultReducer } from "#features/searchResult";
 
 export const store = configureStore({
     reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        
+        searchResult: searchResultReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
 })
